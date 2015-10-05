7d679f6c5e7d6289b3389910dc6f7bea769e6784
Constantin <my.test@live.ca>
2015-10-05 12:25:50 -0400

    Added alignment option to the table cells, and added image watermarks for the pages.

--- src/Zone/Core/Component/PrintContent/DataTableCell.php
+++ src/Zone/Core/Component/PrintContent/DataTableCell.php
@@ -16,26 +16,38 @@ class DataTableCell
     private $height;
     private $position;
     private $text;
+    private $align;
 
     /**
      * DataTableCell constructor.
+     * @param $text
      * @param $width
      * @param $height
-     * @param $position
-     * @param $text
+     * @param string $align
+     * @param string $position
      */
-    public function __construct($text, $width = null, $height = null, $position = 'top')
+    public function __construct($text, $width = null, $height = null, $align = 'left', $position = 'top')
     {
         $this->width = $width;
         $this->height = $height;
         $this->position = $position;
         $this->text = $text;
+        $this->setAlign($align);
+        return $this;
     }
 
     function compile()
     {
         $width = $this->width ? 'width="' . $this->width . '"' : '';
         $height = $this->height ? 'width="' . $this->height . '"' : '';
-        return sprintf('<td %s %s valign="%s">%s</td>', $width, $height, $this->position, $this->text);
+        return sprintf('<td %s %s valign="%s" align="%s">%s</td>', $width, $height, $this->position, $this->align, $this->text);
+    }
+
+    /**
+     * @param mixed $align
+     */
+    public function setAlign($align)
+    {
+        $this->align = $align;
     }
 }
