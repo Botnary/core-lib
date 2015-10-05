a88cab2e722f0346b87e3a8a31f45657fc42579f
Constantin <my.test@live.ca>
2015-10-05 09:20:48 -0400

    Trying to fix the cell text wrap.

--- src/Zone/Core/Component/PrintContent/Printer.php
+++ src/Zone/Core/Component/PrintContent/Printer.php
@@ -74,7 +74,7 @@ class Printer extends UnicodeFPDF
                 $this->SetFont('', $style);
             }
             //Print the text
-            $this->MultiCell($w, 5, $this->toUtf($data[$i]), 0, $a);
+            $this->MultiCell($w, 5, $this->getWrapped($data[$i],$w), 0, $a);
             //Reset back the style
             $this->SetFont('', '');
             //Put the position to the right of the cell
@@ -103,76 +103,78 @@ class Printer extends UnicodeFPDF
         }
     }
 
-    function nbLines($w, $txt)
+    function nbLines($maxwidth, $text)
     {
-        //Computes the number of lines a MultiCell of width w will take
-        $cw =& $this->CurrentFont['cw'];
-        if ($w == 0)
-            $w = $this->w - $this->rMargin - $this->x;
-        $wmax = ($w - 2 * $this->cMargin) * 1000 / $this->FontSize;
-        $s = str_replace("\r", '', $txt);
-        $nb = mb_strlen($s);
-        if ($nb > 0 and $s[$nb - 1] == "\n")
-            $nb--;
-        $sep = -1;
-        $i = 0;
-        $j = 0;
-        $l = 0;
-        $nl = 1;
-        while ($i < $nb) {
-            $c = $s[$i];
-            if ($c == "\n") {
-                $i++;
-                $sep = -1;
-                $j = $i;
-                $l = 0;
-                $nl++;
-                continue;
+        $text = trim($text);
+        if ($text === '')
+            return 0;
+        $space = $this->GetStringWidth(' ');
+        $lines = explode("\n", $text);
+        $text = '';
+        $count = 0;
+
+        foreach ($lines as $line) {
+            $words = preg_split('/ +/', $line);
+            $width = 0;
+
+            foreach ($words as $word) {
+                $wordwidth = $this->GetStringWidth($word);
+                if ($wordwidth > $maxwidth) {
+                    // Word is too long, we cut it
+                    for ($i = 0; $i < strlen($word); $i++) {
+                        $wordwidth = $this->GetStringWidth(substr($word, $i, 1));
+                        if ($width + $wordwidth <= $maxwidth) {
+                            $width += $wordwidth;
+                            $text .= substr($word, $i, 1);
+                        } else {
+                            $width = $wordwidth;
+                            $text = rtrim($text) . "\n" . substr($word, $i, 1);
+                            $count++;
+                        }
+                    }
+                } elseif ($width + $wordwidth <= $maxwidth) {
+                    $width += $wordwidth + $space;
+                    $text .= $word . ' ';
+                } else {
+                    $width = $wordwidth + $space;
+                    $text = rtrim($text) . "\n" . $word . ' ';
+                    $count++;
+                }
             }
-            if ($c == ' ')
-                $sep = $i;
-            $l += $cw[$c];
-            if ($l > $wmax) {
-                if ($sep == -1) {
-                    if ($i == $j)
-                        $i++;
-                } else
-                    $i = $sep + 1;
-                $sep = -1;
-                $j = $i;
-                $l = 0;
-                $nl++;
-            } else
-                $i++;
+            $text = rtrim($text) . "\n";
+            $count++;
         }
-        return $nl;
+        return $count;
     }
 
-    function MultiCell($w, $h, $txt, $border = 0, $align = 'J', $fill = false)
+    function MultiCell($w, $h, $txt, $border=0, $align='J', $fill=false)
     {
         // Output text with automatic or explicit line breaks
         $cw = &$this->CurrentFont['cw'];
-        if ($w == 0)
-            $w = $this->w - $this->rMargin - $this->x;
-        //$wmax = ($w-2*$this->cMargin)*1000/$this->FontSize;
-        $wmax = $this->getAvailableWidth();
-        $s = str_replace("\r", '', $txt);
+        if($w==0)
+            $w = $this->w-$this->rMargin-$this->x;
+        $wmax = ($w-2*$this->cMargin)*1000/$this->FontSize;
+        $s = str_replace("\r",'',$txt);
         $nb = mb_strlen($s);
-        if ($nb > 0 && $s[$nb - 1] == "\n")
+        if($nb>0 && $s[$nb-1]=="\n")
             $nb--;
         $b = 0;
-        if ($border) {
-            if ($border == 1) {
+        if($border)
+        {
+            if($border==1)
+            {
                 $border = 'LTRB';
                 $b = 'LRT';
                 $b2 = 'LR';
-            } else {
+            }
+            else
+            {
                 $b2 = '';
-                if (strpos($border, 'L') !== false)
+                if(strpos($border,'L')!==false)
                     $b2 .= 'L';
-                if (strpos($border, 'R') !== false)
+                if(strpos($border,'R')!==false)
                     $b2 .= 'R';
-                $b = (strpos($border, 'T') !== false) ? $b2 . 'T' : $b2;
+                $b = (strpos($border,'T')!==false) ? $b2.'T' : $b2;
             }
         }
         $sep = -1;
@@ -181,68 +183,80 @@ class Printer extends UnicodeFPDF
         $l = 0;
         $ns = 0;
         $nl = 1;
-        while ($i < $nb) {
+        while($i<$nb)
+        {
             // Get next character
             $c = $s[$i];
-            if ($c == "\n") {
+            if($c=="\n")
+            {
                 // Explicit line break
-                if ($this->ws > 0) {
+                if($this->ws>0)
+                {
                     $this->ws = 0;
                     $this->_out('0 Tw');
                 }
-                $this->Cell($w, $h, substr($s, $j, $i - $j), $b, 2, $align, $fill);
+                $this->Cell($w,$h,substr($s,$j,$i-$j),$b,2,$align,$fill);
                 $i++;
                 $sep = -1;
                 $j = $i;
                 $l = 0;
                 $ns = 0;
                 $nl++;
-                if ($border && $nl == 2)
+                if($border && $nl==2)
                     $b = $b2;
                 continue;
             }
-            if ($c == ' ') {
+            if($c==' ')
+            {
                 $sep = $i;
                 $ls = $l;
                 $ns++;
             }
             $l += $cw[$c];
-            if ($l > $wmax) {
+            if($l>$wmax)
+            {
                 // Automatic line break
-                if ($sep == -1) {
-                    if ($i == $j)
+                if($sep==-1)
+                {
+                    if($i==$j)
                         $i++;
-                    if ($this->ws > 0) {
+                    if($this->ws>0)
+                    {
                         $this->ws = 0;
                         $this->_out('0 Tw');
                     }
-                    $this->Cell($w, $h, substr($s, $j, $i - $j), $b, 2, $align, $fill);
-                } else {
-                    if ($align == 'J') {
-                        $this->ws = ($ns > 1) ? ($wmax - $ls) / 1000 * $this->FontSize / ($ns - 1) : 0;
-                        $this->_out(sprintf('%.3F Tw', $this->ws * $this->k));
+                    $this->Cell($w,$h,substr($s,$j,$i-$j),$b,2,$align,$fill);
+                }
+                else
+                {
+                    if($align=='J')
+                    {
+                        $this->ws = ($ns>1) ? ($wmax-$ls)/1000*$this->FontSize/($ns-1) : 0;
+                        $this->_out(sprintf('%.3F Tw',$this->ws*$this->k));
                     }
-                    $this->Cell($w, $h, substr($s, $j, $sep - $j), $b, 2, $align, $fill);
-                    $i = $sep + 1;
+                    $this->Cell($w,$h,substr($s,$j,$sep-$j),$b,2,$align,$fill);
+                    $i = $sep+1;
                 }
                 $sep = -1;
                 $j = $i;
                 $l = 0;
                 $ns = 0;
                 $nl++;
-                if ($border && $nl == 2)
+                if($border && $nl==2)
                     $b = $b2;
-            } else
+            }
+            else
                 $i++;
         }
         // Last chunk
-        if ($this->ws > 0) {
+        if($this->ws>0)
+        {
             $this->ws = 0;
             $this->_out('0 Tw');
         }
-        if ($border && strpos($border, 'B') !== false)
+        if($border && strpos($border,'B')!==false)
             $b .= 'B';
-        $this->Cell($w, $h, substr($s, $j, $i - $j), $b, 2, $align, $fill);
+        $this->Cell($w,$h,substr($s,$j,$i-$j),$b,2,$align,$fill);
         $this->x = $this->lMargin;
     }
 
