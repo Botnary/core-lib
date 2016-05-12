3df6634e170e13c56c9435446edd759c5526b0d2
Constantin <my.test@live.ca>
2016-05-12 10:01:08 -0400

    MPDF changed the name of the constructor.

--- src/Zone/Core/Component/PrintContent/Printer.php
+++ src/Zone/Core/Component/PrintContent/Printer.php
@@ -38,7 +38,7 @@ class Printer extends \mPDF
      */
     public function __construct($mode = '', $format = 'A4', $default_font_size = 0, $default_font = '', $mgl = 15, $mgr = 15, $mgt = 16, $mgb = 16, $mgh = 9, $mgf = 9, $orientation = 'P')
     {
-        parent::mPDF($mode, $format, $default_font_size, $default_font, $mgl, $mgr, $mgt, $mgb, $mgh, $mgf, $orientation);
+        parent::__construct($mode, $format, $default_font_size, $default_font, $mgl, $mgr, $mgt, $mgb, $mgh, $mgf, $orientation);
         $this->eventDispatcher = new EventDispatcher();
     }
 
