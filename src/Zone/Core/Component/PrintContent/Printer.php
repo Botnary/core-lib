61598f531ce76a62ff896ed18ef77bca63836ac9
Constantin <my.test@live.ca>
2019-05-18 10:38:08 -0400

    Adjusting mPDF constructor.

--- src/Zone/Core/Component/PrintContent/Printer.php
+++ src/Zone/Core/Component/PrintContent/Printer.php
@@ -36,10 +36,17 @@ class Printer extends \Mpdf\Mpdf
      * @param int $mgh
      * @param int $mgf
      * @param string $orientation
+     * @throws \Mpdf\MpdfException
      */
     public function __construct($mode = '', $format = 'A4', $default_font_size = 0, $default_font = '', $mgl = 15, $mgr = 15, $mgt = 16, $mgb = 16, $mgh = 9, $mgf = 9, $orientation = 'P')
     {
-        parent::__construct($mode, $format, $default_font_size, $default_font, $mgl, $mgr, $mgt, $mgb, $mgh, $mgf, $orientation);
+        parent::__construct([
+            'format' => $format,
+            'mode' => $mode,
+            'orientation' => $orientation,
+
+        ]);
+        //parent::__construct($mode, $format, $default_font_size, $default_font, $mgl, $mgr, $mgt, $mgb, $mgh, $mgf, $orientation);
         $this->eventDispatcher = new EventDispatcher();
     }
 
